fb9e2beb1769624a90e792c5db48c91ca0cf8853
Alexzander <alexzander721@mail.ru>
2024-01-25 16:22:09 +0300

    Update Import_Text.py
    New import algorithm

--- Import_Text.py
+++ Import_Text.py
@@ -1,13 +1,20 @@
 import os
 import os.path
 
-from PyQt5.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
+from PyQt5.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QVariant
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QAction, QFileDialog, QMessageBox
-from qgis.core import QgsProject, QgsVectorLayer, QgsVectorFileWriter
+from qgis.core import QgsProject, QgsVectorLayer, QgsVectorFileWriter, QgsField, QgsFeature, QgsPointXY, QgsGeometry
 
-from .resources import *
 from .Import_Text_dialog import TextWingisDialog
+from .resources import *
+
+
+def message(title, text):
+    proj_msg = QMessageBox()
+    proj_msg.setWindowTitle(title)
+    proj_msg.setText(text)
+    proj_msg.exec_()
 
 
 class TextWingis:
@@ -15,6 +22,7 @@ class TextWingis:
     def __init__(self, iface):
 
         self.iface = iface
+        self.instance = QgsProject.instance()
         self.plugin_dir = os.path.dirname(__file__)
         locale = QSettings().value('locale/userLocale')[0:2]
         locale_path = os.path.join(
@@ -60,7 +68,6 @@ class TextWingis:
             action.setWhatsThis(whats_this)
 
         if add_to_toolbar:
-            # Adds plugin icon to Plugins toolbar
             self.iface.addToolBarIcon(action)
 
         if add_to_menu:
@@ -73,15 +80,12 @@ class TextWingis:
         return action
 
     def initGui(self):
-
         icon_path = ':/plugins/Import_Text/icon.png'
         self.add_action(
             icon_path,
             text=self.tr(u'Import Text from WinGis'),
             callback=self.run,
             parent=self.iface.mainWindow())
-
-        # will be set False in run()
         self.first_start = True
 
     def unload(self):
@@ -92,80 +96,39 @@ class TextWingis:
             self.iface.removeToolBarIcon(action)
 
     def select_file(self):
-        filename = QFileDialog.getOpenFileNames(self.dlg, 'Open file', '', '"Файл "MIF" (*.MIF *.MID)')[0]
-        for singlefile in filename:
-            self.dlg.textEdit.append(singlefile)
+        filename = QFileDialog.getOpenFileNames(self.dlg, 'Open file', '', '"Файл "MIF" (*.MIF)')[0]
+        [self.dlg.textEdit.append(singlefile) for singlefile in filename]
+
+    def import_integers(self):
+        filename = self.dlg.textEdit.toPlainText().split('\n')
+        self.dlg.textEdit.clear()
+        for mif in filename:
+            labels = QgsVectorLayer("Point?crs=", f"{mif.split('/')[len(mif.split('/')) - 1][:-4]}", "memory")
+            labels.dataProvider().addAttributes([QgsField(f"Text", QVariant.String)]), labels.updateFields()
+            labels.setCrs(self.instance.crs())
+            with open(mif, "r", encoding='cp1251') as file:
+                text = file.readlines()
+                for index in [i + 1 for i, ltr in enumerate(text) if ltr == "Text\n"]:
+                    if text[index] not in ['    "/"\n', '    "\\"\n']:
+                        value = text[index].replace('"', '').replace('    ', '').replace('n\\', ' ')[:-1]
+                        coord = text[index + 1].replace('    ', '')[:-1].split(' ')
+                        feat = QgsFeature()
+                        feat.setAttributes([value])
+                        x = (float(coord[0]) + float(coord[2])) / 2
+                        y = (float(coord[1]) + float(coord[3])) / 2
+                        feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(x, y)))
+                        labels.dataProvider().addFeatures([feat])
+                QgsProject.instance().addMapLayer(labels)
+                labels.loadNamedStyle(os.path.join(os.path.dirname(__file__), "Style.qml"))
 
     def run(self):
-
-        if self.first_start == True:
+        if self.first_start:
             self.first_start = False
             self.dlg = TextWingisDialog()
             self.dlg.pushButton.clicked.connect(self.select_file)
-
         self.dlg.show()
-
         result = self.dlg.exec_()
-
         if result:
-            filename = self.dlg.textEdit.toPlainText()
-            self.dlg.textEdit.clear()
-            spisok = filename.split('\n')
-            for fi in spisok:
-                with open(fi, "r", encoding='cp1251') as file:
-                    text = file.readlines()
-                    for wrong in text:
-                        if 'Region ' in wrong:
-                            proj_msg = QMessageBox()
-                            proj_msg.setWindowTitle("Warning")
-                            proj_msg.setText(
-                                "Слой содержит не корректную геометрию (линии, полигоны)!\n"
-                                "Слой должен содержать только подписи (точки), исправьте геометрию.")
-                            proj_msg.exec_()
-                            break
-                        elif 'Pline ' in wrong:
-                            proj_msg = QMessageBox()
-                            proj_msg.setWindowTitle("Warning")
-                            proj_msg.setText(
-                                "Слой содержит не корректную геометрию (линии, полигоны)!\n"
-                                "Слой должен содержать только подписи (точки), исправьте геометрию.")
-                            proj_msg.exec_()
-                            break
-                    text.pop(1)
-                    # Замена набора символов
-                    text.insert(1, 'Charset "WindowsCyrillic"\n')
-                    # Находим индекс сингнального слова "Text" и добавляем их в список
-                    x = [i for i, ltr in enumerate(text) if ltr == "Text\n"]
-                    # print(x)
-                    # Увеличиваем значение индекса на 1, таким образом мы получим уже индекс самих надписей
-                    # Создадим список значений
-                    lst_label = []
-                    for ind in x:
-                        trind = ind + 1
-                        label = text[trind]
-                        # Уберём лишние знаки
-                        lst_label.append(label[4:])
-                        # print(label[4:-1])
-                    # print(lst_label)
-                # запись полученых значений
-                with open(fi[:-4] + ".MID", 'w', encoding='cp1251') as f:
-                    f.writelines(lst_label)
-                # Смена кодировки
-                with open(fi, "w", encoding='cp1251') as file:
-                    file.writelines(text)
-                path_to_airports_layer = fi
-                layername = fi.rpartition('/')[2]
-                layername = layername[:-4]
-                vlayer = QgsVectorLayer(path_to_airports_layer, layername, "ogr",
-                                        crs=QgsProject.instance().crs())
-                if not vlayer.isValid():
-                    print("Layer failed to load!")
-                else:
-                    # Открытие слоя
-                    QgsProject.instance().addMapLayer(vlayer)
-                    save_options = QgsVectorFileWriter.SaveVectorOptions()
-                    # Добавление стиля для подписей
-                    style_path = os.path.join(os.path.dirname(__file__), "Style.qml")
-                    (errorMsg, result) = vlayer.loadNamedStyle(style_path)
+            self.import_integers()
         else:
             self.dlg.textEdit.clear()
